608fdd1b6e593afc6da35dfbf77fa0155160f22e
Kaue <kauezinho_naruto@hotmail.com>
2023-07-16 16:50:28 -0300

    Alterando varios componentes

--- js/main.js
+++ js/main.js
@@ -6,14 +6,15 @@ const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 const controle = document.querySelectorAll(".controle-ajuste")
 
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+})
 
-
-somar.addEventListener("click", () => {manipulaDados("somar")})
-
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
 
 function manipulaDados(operacao) {
-    if(operacao === "subtrair") {
+    if(operacao === "-") {
         braco.value = parseInt(braco.value) - 1;
     } else {
         braco.value = parseInt(braco.value) + 1;
