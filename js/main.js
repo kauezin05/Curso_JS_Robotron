4614683c22d8f5937f1e83b36a7476d879fcfb3b
Kaue <kauezinho_naruto@hotmail.com>
2023-07-16 16:08:45 -0300

    Reaproveitando código

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
-
+/*querySelectorAll = busca todos os elementos com o mesmo parametro de busca*/
 
 const robotron = document.querySelector("#robotron")
 const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
+const controle = document.querySelectorAll(".controle-ajuste")
 
 
 
-somar.addEventListener("click", (evento) => {
-    
-    braco.value = parseInt(braco.value) + 1;
-})
-subtrair.addEventListener("click", (evento) => {
-    
-    braco.value = parseInt(braco.value) - 1;
-})
+somar.addEventListener("click", () => {manipulaDados("somar")})
 
+subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
+
+function manipulaDados(operacao) {
+    if(operacao === "subtrair") {
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
+}
 
 // /* QUALQUER PONTO QUE TIVER UMA INTERAÇÃO DE CLICK, PEGARA AS INFORMAÇÕES DE ONDE FOI CLICADO, PASSARA COMO PARAMETRO E EXIBIRÁ ESSE PARAMETRO */
 // robotron.addEventListener("click", (evento) => {
