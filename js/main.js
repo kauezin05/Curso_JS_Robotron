7b92eb7734d503fde6687afda3e13019d37192b3
Kaue <kauezinho_naruto@hotmail.com>
2023-07-16 18:05:44 -0300

    Alterando o elemento

--- js/main.js
+++ js/main.js
@@ -8,16 +8,19 @@ const controle = document.querySelectorAll(".controle-ajuste")
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
     })
 })
 
 
-function manipulaDados(operacao) {
+function manipulaDados(operacao, controle) {
+
+    const peca = controle.querySelector(".controle-contador")
+
     if(operacao === "-") {
-        braco.value = parseInt(braco.value) - 1;
+        peca.value = parseInt(peca.value) - 1;
     } else {
-        braco.value = parseInt(braco.value) + 1;
+        peca.value = parseInt(peca.value) + 1;
     }
 }
 
