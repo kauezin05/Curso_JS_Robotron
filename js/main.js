5c0258f393391a0e454ce541350a27347b36eb33
Kaue <kauezinho_naruto@hotmail.com>
2023-07-16 15:25:19 -0300

    Alterando quantidade de itens na pagina

--- js/main.js
+++ js/main.js
@@ -1,20 +1,37 @@
-/*FUNÇÃO DE ALERTA DE OI */
+
 
 const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", alertaOI);
 
-function alertaOI() {
-    alert("oi");
-}
 
-/* QUALQUER PONTO QUE TIVER UMA INTERAÇÃO DE CLICK, PEGARA AS INFORMAÇÕES DE ONDE FOI CLICADO, PASSARA COMO PARAMETRO E EXIBIRÁ ESSE PARAMETRO */
-robotron.addEventListener("click", (evento) => {
-    console.log(evento)
+somar.addEventListener("click", (evento) => {
+    
+    braco.value = parseInt(braco.value) + 1;
+})
+subtrair.addEventListener("click", (evento) => {
+    
+    braco.value = parseInt(braco.value) - 1;
 })
 
 
-/*ARROW FUNCTION COM ALERT DE BEM VINDO */
-window.addEventListener("load", () => {
-    alert("Bem-vindo ao Robotron 2000");
-});
+// /* QUALQUER PONTO QUE TIVER UMA INTERAÇÃO DE CLICK, PEGARA AS INFORMAÇÕES DE ONDE FOI CLICADO, PASSARA COMO PARAMETRO E EXIBIRÁ ESSE PARAMETRO */
+// robotron.addEventListener("click", (evento) => {
+//     console.log(evento)
+// })
+
+
+// /*ARROW FUNCTION COM ALERT DE BEM VINDO */
+// window.addEventListener("load", () => {
+//     alert("Bem-vindo ao Robotron 2000");
+// });
+
+// /*FUNÇÃO DE ALERTA DE OI */
+
+// robotron.addEventListener("click", alertaOI);
+
+// function alertaOI() {
+//     alert("oi");
+// }
